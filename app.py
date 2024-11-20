1c809fc0223b767db586e7ecd48f6b142766d870
abduldattijo <abduldattijo1@gmail.com.com>
2024-11-21 01:26:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,14 @@ def create_app():
     app = Flask(__name__)
 
     # Database configuration
-    
-
-    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:axrjuGyXsVBvXDSxkKZVtpqAseqwPcTE@junction.proxy.rlwy.net:44492/railway")
-    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-
+    #render production database
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://podcast_db_n1bc_user:sKSceAGVCbVYN4QH4rWIi6CxdEqMFnUC@dpg-csuvrkd6l47c7383de6g-a/podcast_db_n1bc')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+     
+    #railway production database
+    #DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:axrjuGyXsVBvXDSxkKZVtpqAseqwPcTE@junction.proxy.rlwy.net:44492/railway")
+    #app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+     #local database
     #DATABASE_URI = 'postgresql+psycopg2://postgres:podcast@localhost/podcast_db'
     #app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False   
