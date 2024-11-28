87879c5f2dde9cf788474b739d545efeb2a3ba9f
abduldattijo <abduldattijo1@gmail.com.com>
2024-11-29 05:02:07 +0900

    longer time i think to progress bar

--- static/js/upload.js
+++ static/js/upload.js
@@ -229,7 +229,7 @@ function toggleClientInput() {
                 const response = await fetch(form.action, {
                     method: 'POST',
                     body: formData,
-                    timeout: 3000000  // 50 minutes timeout
+                    timeout: 30000000  // 50 minutes timeout
                 });
  
                 clearInterval(interval);
